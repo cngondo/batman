4ee9af07ce669d4dc2813430fb684229e4289f28
cngondo <devngondo@gmail.com>
2016-08-17 16:41:59 +0300

    UI components

--- app/src/main/java/superman/example/com/superman/SigninActivity.java
+++ app/src/main/java/superman/example/com/superman/SigninActivity.java
@@ -1,13 +1,75 @@
 package superman.example.com.superman;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.PendingIntent;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SigninActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+
+public class SigninActivity extends FragmentActivity implements ConnectionCallbacks,
+        OnConnectionFailedListener, View.OnClickListener {
+    // Constants
+    private static final int SIGNED_IN = 0;
+    private static final int STATE_SIGNING_IN = 1;
+    private static final int STATE_IN_PROGRESS = 2;
+    private static final int RC_SIGN_IN = 0;
+
+    //Object references for API Client
+    private GoogleApiClient mGoogleApiClient;
+    private int mSignInProgress;
+    private PendingIntent mSignInIntent;
+
+    //References for the Views
+    private SignInButton mSignInButton;
+    private Button mSignOutButton;
+    private Button mRevokeButton;
+    private TextView mStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        //all views
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignOutButton = (Button) findViewById(R.id.sign_out_button);
+        mRevokeButton = (Button) findViewById(R.id.revoke_access_button);
+        mStatus =(TextView) findViewById(R.id.statuslabel);
+
+        //click listeners for the button
+        mSignInButton.setOnClickListener(this);
+        mSignOutButton.setOnClickListener(this);
+        mRevokeButton.setOnClickListener(this);
+
+        //google API Client
+        mGoogleApiClient = buildGoogleAPIClient();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
